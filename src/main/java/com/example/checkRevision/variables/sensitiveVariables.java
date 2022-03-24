7dc9f4c0d855b2c40003ad0f38d2df6732112d5c
haathimPC <haathimmunas@gmail.com>
2022-03-24 11:52:57 +0530

    added variable for azure

---
+++ src/main/java/com/example/checkRevision/variables/sensitiveVariables.java
@@ -0,0 +1,8 @@
+package com.example.checkRevision.variables;
+
+public class sensitiveVariables {
+//    public static String azureConnectionStr = "DefaultEndpointsProtocol=https;AccountName=javacodeimageupload;AccountKey=S8Gcx6QY1i5g4Kz/D/sCKE8bFASIRRBtAXfKfKR3i+yC2On2+c0/VANMfeGl2/dCi3v10ZxlATKeSiRwCsshAw==;EndpointSuffix=core.windows.net";
+
+
+    public static String azureConnectionStr = "DefaultEndpointsProtocol=https;AccountName=hats;AccountKey=1l4ChsZaTEPRjcSdgIETU9rVIpKpLKDx3yCj5hqu2OqzvWaKl53O955ea3wzKI9YRCuPbQzUVaZj+AStVzzTNw==;EndpointSuffix=core.windows.net";
+}
